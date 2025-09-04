e96c4bf9df899ce9906c5108cb4c32b83632f07a
Neckro1 <f.rafael03@escola.pr.gov.br>
2025-09-04 10:59:20 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 const reiniciarBtn = document.getElementById('btn-reiniciar')
 
-avanca.forEach(button =>(
-    button.addEventListener('click')
-)
+avanca.forEach(button =>{
+  button.addEventListener('click',function() {
+    const atual = document.querySelector('.ativo');
+    const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
+    
+    atual.classList.remove('ativo');
+    const proximoelemento = document.getElementById(proximoPasso);
+
+    if (proximoElemento){
+        proxmoelementp.classList.add('arivo');
+    } else {
+        console.error(`Elemento com ID "${proximoPasso}"não encontrado.`)
+    }
+  });
+});
